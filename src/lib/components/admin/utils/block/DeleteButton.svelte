fb3ad592f9fae3718910d582ef5a5c166d0bf047
Thomas Clouet <thomasclouet@wise77.vub.ac.be>
2022-08-11 09:55:57 +0200

    :zap: Deletion popup appears close to the cursor

--- src/lib/components/admin/utils/block/DeleteButton.svelte
+++ src/lib/components/admin/utils/block/DeleteButton.svelte
@@ -8,12 +8,35 @@
 	let confirmDeletion = false;
 	let coordinates = { x: 0, y: 0 };
 
+	// TODO LORS DU CLIC GET LES COORDS ET OUVRIR A CETTE ENDROIT
+
 	/**
 	 * Function used to remove an object from Firebase
 	 */
 	async function removeObject(ref) {
 		if (ref) await deleteDoc(ref);
 	}
+
+	/**
+	 * Function to show the popup close to the cursor
+	 * if it's too close from the page limit we add an offset
+	 * @param e
+	 */
+	const setCoordinates = (e) => {
+		let offsetX = 0;
+		let offsetY = 0;
+
+		console.log(e);
+		if (e.x + 200 > window.innerWidth) {
+			offsetX = 192;
+		}
+		if (e.y + 200 > window.innerHeight) {
+			offsetY = 128;
+		}
+
+		coordinates.x = e.x - offsetX;
+		coordinates.y = e.y - offsetY;
+	};
 </script>
 
 <p
@@ -21,7 +44,8 @@
         text-center rounded-full cursor-pointer
 		transition-colors
         bg-red-300 hover:bg-red-500"
-	on:click={() => {
+	on:click={(e) => {
+		setCoordinates(e);
 		confirmDeletion = !confirmDeletion;
 	}}
 	use:clickOutside
@@ -32,9 +56,9 @@
 
 <div
 	class="fixed h-32 w-48
-		bg-white border-2 border-c-black
+		bg-white border-2 border-c-black position-to-cursor
 		{confirmDeletion ? 'visible' : 'invisible'}"
-	style="top:{coordinates.y};left:{coordinates.x};"
+	style="--y:{coordinates.y}px;--x:{coordinates.x}px;"
 >
 	<p class="w-full text-center underline">Confirm deletion</p>
 	<div class="flex w-full h-full">
@@ -60,3 +84,10 @@
 		</p>
 	</div>
 </div>
+
+<style>
+	.position-to-cursor {
+		top: var(--y);
+		left: var(--x);
+	}
+</style>
